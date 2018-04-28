af2ba6b274cfc8e8d8eb0f8c63b73543e7b6f730
Elias Akawako <Elikescoding@outlook.com>
2018-04-28 18:06:07 +0100

    I've update the console app with some try catches but I would like to add more error handling in the future

--- DemoConsoleUI/Program.cs
+++ DemoConsoleUI/Program.cs
@@ -11,93 +11,128 @@ namespace DemoConsoleUI
         static void Main(string[] args)
         {
 
-            //Calling my person from outside the static void main method because EACH METHOD SHOULD ONLY HAVE ONE FUNCTION
-            //CreateMyPerson();
+            try
+            {
+                
+                //Calling my person from outside the static void main method because EACH METHOD SHOULD ONLY HAVE ONE FUNCTION
+                //CreateMyPerson();
 
-            //Calling my sample 
-            //SetUpSampleData();
+                //Calling my sample 
+                //SetUpSampleData();
 
-            //Calling my greet function
-            //GreetAllPeople();
+                //Calling my greet function
+                //GreetAllPeople();
 
-            //Create An Array and Loop through the contents
-            //TeachMeArrays();
+                //Create An Array and Loop through the contents
+                //TeachMeArrays();
 
-            //Create a stack and Loop through the contents of the stack
-            //CreateMyStack();
+                //Create a stack and Loop through the contents of the stack
+                //CreateMyStack();
 
-            //Create a Queue and Loop through the contents
-            //CreateMyQueue();
+                //Create a Queue and Loop through the contents
+                //CreateMyQueue();
 
-            //Create a dictionary and display the contents 
-            //CreateMyDictionary();
+                //Create a dictionary and display the contents 
+                //CreateMyDictionary();
 
-            //Call an if statement
-            //DothisIf();
+                //Call an if statement
+                //DothisIf();
 
-            //Call an if else statement
-            //DoThisElse();
+                //Call an if else statement
+                //DoThisElse();
 
-            //Call my else-if-else statement
-            //DoThisElseIfElse();
+                //Call my else-if-else statement
+                //DoThisElseIfElse();
 
-            //Call my Case-switch statement
-            //CreateCaseSwitch();
+                //Call my Case-switch statement
+                //CreateCaseSwitch();
 
-            //Call a for loop
-            //CreateAForLoop();
+                //Call a for loop
+                //CreateAForLoop();
 
-            //Call a while loop
-            //CreateAWhileLoop();
+                //Call a while loop
+                //CreateAWhileLoop();
 
-            //Call a Do-While Loop
-            //CreateADoWhileLoop();
+                //Call a Do-While Loop
+                //CreateADoWhileLoop();
 
-            //Get my string builder
-            //StringBuilderTest();
+                //Get my string builder
+                //StringBuilderTest();
 
-            //Call my string concat
-            //StringConcat();
+                //Call my string concat
+                //StringConcat();
 
-            //Call My Ternary Conditon
-            //TeachMeTernarys();
+                //Call My Ternary Conditon
+                //TeachMeTernarys();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
 
-            //Tell me how long it took to perform everything
-            Console.WriteLine("Total Time Taken is :" + DateTime.Now.Second + " Seconds");
-            Console.ReadLine();
+                var ix = ex.InnerException;
+
+                while (ix != null)
+                {
+                    Console.WriteLine(ix.StackTrace);
+                    ix = ix.InnerException;
+                };
+            }
+            finally
+            {
+                //Tell me how long it took to perform everything
+                Console.WriteLine("Total Time Taken is :" + DateTime.Now.Second + " Seconds");
+                Console.ReadLine();
+            }
         }
 
         //Concatinate a string
         private static void StringConcat()
         {
-            //Say hello to someone from my person using old string contact
-            //string test = "Hello" + person.firstName + "" + person.lastName + "!";
+            try
+            {
+                //Say hello to someone from my person using old string contact
+                //string test = "Hello" + person.FirstName + "" + person.LastName + "!";
 
-            //Say hello to a person using the new string concat
-            //string betterTest = $"Hello {person2.firstName} {person2.lastName} !";
+                //Say hello to a person using the new string concat
+                //string betterTest = $"Hello {person2.FirstName} {person2.LastName} !";
+            }
+            catch (Exception ex)
+            {
+
+                throw new ArgumentException("Please check your string", ex);
+            }
         }
 
         //Test for my string builder
         private static void StringBuilderTest()
         {
-            //Create a counter, my string and a string builder
-            DateTime clock = new DateTime();
-            string speakFam = "Original String";
-            StringBuilder Ns = new StringBuilder();
-
-            //Tell me how long it has taken to build my objects 
-            Console.WriteLine("Time now is :" + clock.Second + " Seconds");
-            Console.WriteLine(Ns.ToString());
-
-            //Loop through my string object and append a string to to it
-            for (int i = 0; i < 100; i++)
+            try
             {
-                Console.WriteLine(speakFam + Ns.Append("New String + "));
-            }
+                //Create a counter, my string and a string builder
+                DateTime clock = new DateTime();
+                string speakFam = "Original String";
+                StringBuilder Ns = new StringBuilder();
 
-            //Tell when loop has finished and how long it has taken
-            Console.WriteLine("No more strings to add");
-            Console.WriteLine("Adding 100 strings to " + speakFam + " took an extra :" + clock.Second + "  Seconds");
+                //Tell me how long it has taken to build my objects 
+                Console.WriteLine("Time now is :" + clock.Second + " Seconds");
+                Console.WriteLine(Ns.ToString());
+
+                //Loop through my string object and append a string to to it
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(speakFam + Ns.Append("New String + "));
+                }
+
+                //Tell when loop has finished and how long it has taken
+                Console.WriteLine("No more strings to add");
+                Console.WriteLine("Adding 100 strings to " + speakFam + " took an extra :" + clock.Second + "  Seconds");
+            }
+            catch (Exception ex)
+            {
+
+                throw new ArgumentException("Please check if your string is null", ex);
+            }
 
         }
 
@@ -107,26 +142,35 @@ namespace DemoConsoleUI
         //Create person
         private static void CreateMyPerson()
         {
-            //NEW Person TEMPLATE 
-            Person newPerson = new Person();
+            try
+            {
+                //NEW Person TEMPLATE 
+                Person newPerson = new Person();
 
-            newPerson.firstName = "TIMMYYY";
-            newPerson.lastName = "TURRRNEER";
-            newPerson.age = 99;
-            newPerson.email = "suckmydick@hotmail.com";
-            newPerson.balance = 110;
+                newPerson.FirstName = "TIMMYYY";
+                newPerson.LastName = "TURRRNEER";
+                newPerson.Age = 99;
+                newPerson.Email = "suckmydick@hotmail.com";
+                newPerson.Balance = 110;
+                newPerson.IsAdmin = false;
+            }
+            catch (Exception ex)
+            {
+
+                throw new ArgumentException("Please check if you correctly entered your details", ex);
+            }
         }
 
         //Add a list of people to the the Person Class
         private static void SetUpSampleData()
         {
             //Add people to my list
-            people.Add(new Person { firstName = "Jack", lastName = "Sawyer", email = "Fict@hotmail.com", age = 25, balance = 100 });
-            people.Add(new Person { firstName = "Nick", lastName = "Shelby", email = "Fict1@hotmail.com", age = 35, balance = 300 });
-            people.Add(new Person { firstName = "Joe", lastName = "Jones", email = "Fict2@hotmail.com", age = 47, balance = 450 });
-            people.Add(new Person { firstName = "Harry", lastName = "Doe", email = "Fict3@hotmail.com", age = 16, balance = 500 });
-            people.Add(new Person { firstName = "Tim", lastName = "Butcher", email = "Fict4@hotmail.com", age = 28, balance = 900 });
-            people.Add(new Person { firstName = "Jonas", lastName = "Butcher", email = "Fict5@hotmail.com", age = 28, balance = 900 });
+            people.Add(new Person { FirstName = "Jack", LastName = "Sawyer", Email = "Fict@hotmail.com", Age = 25, Balance = 100 });
+            people.Add(new Person { FirstName = "Nick", LastName = "Shelby", Email = "Fict1@hotmail.com", Age = 35, Balance = 300 });
+            people.Add(new Person { FirstName = "Joe", LastName = "Jones", Email = "Fict2@hotmail.com", Age = 47, Balance = 450 });
+            people.Add(new Person { FirstName = "Harry", LastName = "Doe", Email = "Fict3@hotmail.com", Age = 16, Balance = 500 });
+            people.Add(new Person { FirstName = "Tim", LastName = "Butcher", Email = "Fict4@hotmail.com", Age = 28, Balance = 900 });
+            people.Add(new Person { FirstName = "Jonas", LastName = "Butcher", Email = "Fict5@hotmail.com", Age = 28, Balance = 900 });
         }
 
         //Method to greet all my customers 
@@ -136,11 +180,11 @@ namespace DemoConsoleUI
             foreach (var person in people)
             {
                 //Say hello to my people if they exist
-                if (person.firstName != null)
+                if (person.FirstName != null)
                 {
-                    Console.WriteLine($"Hello {person.firstName} {person.lastName}");
+                    Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
                     Console.WriteLine($"These are your details :");
-                    Console.WriteLine($"{person.firstName} is {person.age} years old. Your email is: {person.email} and your balance is {person.balance}");
+                    Console.WriteLine($"{person.FirstName} is {person.Age} years old. Your Email is: {person.Email} and your Balance is {person.Balance}");
 
                 }
                 //Only run this line if nobody exists
@@ -165,13 +209,13 @@ namespace DemoConsoleUI
 
             //Tell me about my string Array
             Console.WriteLine(" This is an Array made of strings ");
-            Console.WriteLine(" The item at Index 2 is: " + stringArray[2]);
-            Console.WriteLine(" The item at Index 4 is : " + stringArray[4]);
+            Console.WriteLine(" The item at Index 2 is: " + stringArray[3]);
+            Console.WriteLine(" The item at Index 4 is : " + stringArray[7]);
 
             //Tell me about my num Array
             Console.WriteLine(" This is an Array made of numbers ");
-            Console.WriteLine(" The item at Index 3 is : " + numArray[3]);
-            Console.WriteLine(" The item at Index 5 is : " + numArray[5]);
+            Console.WriteLine(" The item at Index 3 is : " + numArray[4]);
+            Console.WriteLine(" The item at Index 5 is : " + numArray[6]);
 
             //Tell me the conents of my Arrays
             Console.WriteLine(" I will now display the contents of both Arrays ");
@@ -427,10 +471,10 @@ namespace DemoConsoleUI
             Console.WriteLine("Give me a number between 1 and 10 ? :)");
             int myInput = Convert.ToInt32(Console.ReadLine());
 
-            string myClassifer; 
+            string myClassifer;
 
             //Ternary my g 
-            myClassifer = (myInput > 10)?"You picked a BIG number :) " : "You picked a small number :( ";
+            myClassifer = (myInput > 10) ? "You picked a BIG number :) " : "You picked a small number :( ";
 
             //Read out my condition
             Console.WriteLine(myClassifer);
